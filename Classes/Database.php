53c88cd0111a846745b8884556a380ab645cb756
swaema <swaema04@gmail.com>
2025-02-27 00:48:08 +0400

    Re-add Classes/Database.php to the repository

---
+++ Classes/Database.php
@@ -0,0 +1,30 @@
+<?php
+class Database {
+    private static $connection;
+
+    public static function getConnection() {
+        if (!self::$connection) {
+            self::$connection = new mysqli('localhost', 'root', '', 'credit_management');
+            if (self::$connection->connect_error) {
+                die('Database connection failed: ' . self::$connection->connect_error);
+            }
+        }
+        return self::$connection;
+    }
+}
+
+
+
+// class Database {
+//     private static $connection;
+
+//     public static function getConnection() {
+//         if (!self::$connection) {
+//             self::$connection = new mysqli('localhost', 'safefund_swaema', 'Dissertation27$', 'safefund_credit_management');
+//             if (self::$connection->connect_error) {
+//                 die('Database connection failed: ' . self::$connection->connect_error);
+//             }
+//         }
+//         return self::$connection;
+//     }
+// }
