59595df852cc8feb14dbe63c19911672f3ad4b7e
swaema <swaema04@gmail.com>
2025-03-04 21:21:52 +0400

    add chatbot

--- index.php
+++ index.php
@@ -297,3 +297,16 @@ include_once('Layout/carousal.php');
     background-color: white;
 }
 </style>
+<!-- Start of Tawk.to Script -->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/678e56dd3a8427326071adae/1ii206l6u';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!-- End of Tawk.to Script -->
